1ea82096800a0a4a090aed29ef2215acfb363bba
Gregor Kildow <gregor@vt.edu>
2018-09-13 17:05:07 -0400

    Edits to tracking file.

--- src/algorithms/tracking/gnuradio_blocks/dll_pll_veml_tracking.cc
+++ src/algorithms/tracking/gnuradio_blocks/dll_pll_veml_tracking.cc
@@ -881,13 +881,15 @@ void dll_pll_veml_tracking::log_data(bool integrating)
             float tmp_float;
             double tmp_double;
             unsigned long int tmp_long_int;
-            std::chrono::high_resolution_clock::time_point tmp_E_ts, tmp_P_ts, tmp_L_ts;
+            std::chrono::high_resolution_clock::time_point rec_start;
+            int tmp_E_ts, tmp_P_ts, tmp_L_ts;
             int tmp_E_lock, tmp_P_lock, tmp_L_lock;
-            tmp_E_ts = (std::chrono::high_resolution_clock::now() - timer_start);
+            rec_start = (std::chrono::high_resolution_clock::now() - timer_start);
+            tmp_E_ts = static_cast<int>(std::chrono::high_resolution_clock::now() - rec_start) / 1000000;
             tmp_E_lock = static_cast<int>(dll_pll_veml_tracking::preamble_correlated());
-            tmp_P_ts = (std::chrono::high_resolution_clock::now() - timer_start);
+            tmp_P_ts = static_cast<int>(std::chrono::high_resolution_clock::now() - rec_start) / 1000000;
             tmp_P_lock = static_cast<int>(dll_pll_veml_tracking::preamble_correlated());
-            tmp_L_ts = (std::chrono::high_resolution_clock::now() - timer_start);
+            tmp_L_ts = static_cast<int>(std::chrono::high_resolution_clock::now() - rec_start) / 1000000;
             tmp_L_lock = static_cast<int>(dll_pll_veml_tracking::preamble_correlated());
 
             if (trk_parameters.track_pilot)
@@ -926,9 +928,9 @@ void dll_pll_veml_tracking::log_data(bool integrating)
                     tmp_VE = 0.0;
                     tmp_VL = 0.0;
                 }
-            tmp_E = d_E_accu;  //removed abs()
-            tmp_P = d_P_accu;
-            tmp_L = d_L_accu;
+            tmp_E = std::abs<float>(d_E_accu);
+            tmp_P = std::abs<float>(d_P_accu);
+            tmp_L = std::abs<float>(d_L_accu);
             if (integrating)
                 {
                     //TODO: Improve this solution!
@@ -949,13 +951,13 @@ void dll_pll_veml_tracking::log_data(bool integrating)
                     // Dump correlators output
                     d_dump_file.write(reinterpret_cast<char *>(&tmp_VE), sizeof(float));
                     d_dump_file.write(reinterpret_cast<char *>(&tmp_E), sizeof(float));
-                    d_dump_file.write(reinterpret_cast<char *>(&tmp_E_ts), sizeof(std::chrono::high_resolution_clock::time_point));
+                    d_dump_file.write(reinterpret_cast<char *>(&tmp_E_ts), sizeof(int));
                     d_dump_file.write(reinterpret_cast<char *>(&tmp_E_lock), sizeof(int));
                     d_dump_file.write(reinterpret_cast<char *>(&tmp_P), sizeof(float));
-                    d_dump_file.write(reinterpret_cast<char *>(&tmp_P_ts), sizeof(std::chrono::high_resolution_clock::time_point));
+                    d_dump_file.write(reinterpret_cast<char *>(&tmp_P_ts), sizeof(int));
                     d_dump_file.write(reinterpret_cast<char *>(&tmp_P_lock), sizeof(int));
                     d_dump_file.write(reinterpret_cast<char *>(&tmp_L), sizeof(float));
-                    d_dump_file.write(reinterpret_cast<char *>(&tmp_L_ts), sizeof(std::chrono::high_resolution_clock::time_point));
+                    d_dump_file.write(reinterpret_cast<char *>(&tmp_L_ts), sizeof(int));
                     d_dump_file.write(reinterpret_cast<char *>(&tmp_L_lock), sizeof(int));
                     d_dump_file.write(reinterpret_cast<char *>(&tmp_VL), sizeof(float));
                     // PROMPT I and Q (to analyze navigation symbols)
@@ -1010,11 +1012,9 @@ int dll_pll_veml_tracking::save_matfile()
     std::ifstream::pos_type size;
     int number_of_double_vars = 1;
     int number_of_float_vars = 17;
-    int number_of_time_vars = 3;
-    int number_of_int_vars = 3;
+    int number_of_int_vars = 6;
     int epoch_size_bytes = sizeof(unsigned long int) + sizeof(double) * number_of_double_vars +
                            sizeof(float) * number_of_float_vars + sizeof(unsigned int) + 
-                           sizeof(std::chrono::high_resolution_clock::time_point) * number_of_time_vars +
                            sizeof(int) * number_of_int_vars;
     std::ifstream dump_file;
     dump_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
@@ -1041,13 +1041,13 @@ int dll_pll_veml_tracking::save_matfile()
         }
     float *abs_VE = new float[num_epoch];
     float *abs_E = new float[num_epoch];
-    std::chrono::high_resolution_clock::time_point *abs_E_ts = new std::chrono::high_resolution_clock::time_point[num_epoch];
+    int *abs_E_ts = new int[num_epoch];
     int *abs_E_lock = new int[num_epoch];
     float *abs_P = new float[num_epoch];
-    std::chrono::high_resolution_clock::time_point *abs_P_ts = new std::chrono::high_resolution_clock::time_point[num_epoch];
+    int *abs_P_ts = new int[num_epoch];
     int *abs_P_lock = new int[num_epoch];
     float *abs_L = new float[num_epoch];
-    std::chrono::high_resolution_clock::time_point  *abs_L_ts = new std::chrono::high_resolution_clock::time_point[num_epoch];
+    int  *abs_L_ts = new int[num_epoch];
     int *abs_L_lock = new int[num_epoch];
     float *abs_VL = new float[num_epoch];
     float *Prompt_I = new float[num_epoch];
@@ -1074,13 +1074,13 @@ int dll_pll_veml_tracking::save_matfile()
                         {
                             dump_file.read(reinterpret_cast<char *>(&abs_VE[i]), sizeof(float));
                             dump_file.read(reinterpret_cast<char *>(&abs_E[i]), sizeof(float));
-                            dump_file.read(reinterpret_cast<char *>(&abs_E_ts[i]), sizeof(std::chrono::high_resolution_clock::time_point));
+                            dump_file.read(reinterpret_cast<char *>(&abs_E_ts[i]), sizeof(int));
                             dump_file.read(reinterpret_cast<char *>(&abs_E_lock[i]), sizeof(int));
                             dump_file.read(reinterpret_cast<char *>(&abs_P[i]), sizeof(float));
-                            dump_file.read(reinterpret_cast<char *>(&abs_P_ts[i]), sizeof(std::chrono::high_resolution_clock::time_point));
+                            dump_file.read(reinterpret_cast<char *>(&abs_P_ts[i]), sizeof(int));
                             dump_file.read(reinterpret_cast<char *>(&abs_P_lock[i]), sizeof(int));
                             dump_file.read(reinterpret_cast<char *>(&abs_L[i]), sizeof(float));
-                            dump_file.read(reinterpret_cast<char *>(&abs_L_ts[i]), sizeof(std::chrono::high_resolution_clock::time_point));
+                            dump_file.read(reinterpret_cast<char *>(&abs_L_ts[i]), sizeof(int));
                             dump_file.read(reinterpret_cast<char *>(&abs_L_lock[i]), sizeof(int));
                             dump_file.read(reinterpret_cast<char *>(&abs_VL[i]), sizeof(float));
                             dump_file.read(reinterpret_cast<char *>(&Prompt_I[i]), sizeof(float));
